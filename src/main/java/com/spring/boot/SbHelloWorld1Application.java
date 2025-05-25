4b84176f17a7a19bf50787c050c81fdf89bd44f3
ShaikJdk <kannak07@gmail.com>
2025-05-25 17:43:38 +0530

    build success

--- src/main/java/com/spring/boot/SbHelloWorld1Application.java
+++ src/main/java/com/spring/boot/SbHelloWorld1Application.java
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplicatio
+@SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceTransactionManagerAutoConfiguration.class,
 		HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class })
 @EnableAsync
